338ad2acb8539b322e6fd1588677d5d0ce157553
Ismael Kovacevic <ikovacev@htl-steyr.ac.at>
2022-03-04 09:04:54 +0100

    Updated StreamOperation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -12,29 +9,33 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         try {
-            Person person = null;
-            Date d;
-            PhoneNumber phoneNumber = null;
-
             File f = new File("Person.csv");
-            ArrayList<String> arrayList = new ArrayList<>();
+            Writer wr = new FileWriter(f);
+            FileReader fileReader = new FileReader(f);
+            BufferedReader reader = new BufferedReader(fileReader);
+            reader.readLine();
 
-            Scanner sc = new Scanner(f);
-            while (sc.hasNextLine()) {
-                arrayList.add(sc.next());
-            }
-
-
-            for (int i = 1; i < arrayList.size(); i++) {
-                String[] currentStrings = arrayList.get(i).split(";");
-                d = new Date(currentStrings[3]);
-                phoneNumber = new PhoneNumber(currentStrings[4]);
-                person = new Person(currentStrings[0], currentStrings[1], currentStrings[2], d, phoneNumber);
-            }
-            StreamOperation.ToStream(new FileWriter("Person.csv"), person);
+            Person mani =StreamOperation.FromStream(reader);
+            System.out.println(mani);
 
         } catch (FileNotFoundException | IllegalPhoneNumberException | IllegalDateException e) {
             e.printStackTrace();
+            /**
+             * ArrayList<String> arrayList = new ArrayList<>();
+             *
+             *             Scanner sc = new Scanner(f);
+             *             while (sc.hasNextLine()) {
+             *                 arrayList.add(sc.next());
+             *             }
+             *
+             *
+             *             for (int i = 1; i < arrayList.size(); i++) {
+             *                 String[] currentStrings = arrayList.get(i).split(";");
+             *                 d = new Date(currentStrings[3]);
+             *                 phoneNumber = new PhoneNumber(currentStrings[4]);
+             *                 person = new Person(currentStrings[0], currentStrings[1], currentStrings[2], d, phoneNumber);
+             *             }
+             */
         }
     }
 }
